ff1206beffc0c46becdfc52381ddfb4556133457
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-11-06 22:53:59 +0000

    Random fixes

--- components/global/app/AppColorSwitcher.vue
+++ components/global/app/AppColorSwitcher.vue
@@ -1,10 +1,18 @@
 <template>
+  <!-- TODO: Test this. 
+  I probably might need to use
+  https://test-utils.nuxtjs.org/ to incercept
+  $colorModE https://color-mode.nuxtjs.org/ 
+    -->
   <span class="inline-flex">
     <button
       class="rounded-md hover:text-primary-500 focus:outline-none transition ease-in-out duration-150"
       aria-label="Color Mode"
-      @click="$
-      .value === 'dark' ? $colorMode.preference = 'light' : $colorMode.preference = 'dark'"
+      @click="
+        $colorMode.value === 'dark'
+          ? ($colorMode.preference = 'light')
+          : ($colorMode.preference = 'dark')
+      "
     >
       <client-only>
         <IconSun v-if="$colorMode.value === 'light'" class="w-6 h-6" />
@@ -13,6 +21,3 @@
     </button>
   </span>
 </template>
-// TODO: Test this.
-//        I probably might need to use https://test-utils.nuxtjs.org/ to incercept
-//        $colorMode https://color-mode.nuxtjs.org/
